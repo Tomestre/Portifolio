c555736c131e0cb21961433867fcfaf111997d82
Tomestre <gabrielveleci@gmail.com>
2022-12-22 00:08:19 -0300

    buttons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import cerebro from './assets/cerebro.png'
-import { AiOutlineWhatsApp, AiOutlineMobile, AiOutlineGithub } from "react-icons/ai";
+import { AiOutlineWhatsApp, AiOutlineMobile, AiOutlineGithub, AiOutlineMenu } from "react-icons/ai";
+import {SiJavascript, SiReact, SiFigma, SiHtml5, SiAdobephotoshop, SiAdobeillustrator, SiCss3} from "react-icons/si"
 import { Link, animateScroll as scroll } from "react-scroll";
+import { useState } from 'react';
 
 
 
 function App() {
-  
+
+  //Projetos do GitHub
   const itensProjeto = [
     {
     titulo: 'Projeto 1',
@@ -49,38 +52,157 @@ function App() {
         }
        
   ]
-  const coder = '<Coder>'
+
+// Conhecimentos
+const coderSkill = [
+  {
+  titulo:'JavaScript',
+  icone:<SiJavascript/>
+  },
+  {
+  titulo:'React',
+  icone:<SiReact/>
+  },
+  {
+  titulo:'React Native',
+  icone:<SiReact/>
+  },
+
+]
+
+const designerSkill = [
+  {
+    titulo:'Figma',
+    icone:<SiFigma/>
+  },
+  {
+    titulo:'HTML',
+    icone:<SiHtml5/>
+  },
+  {
+    titulo:'CSS',
+    icone:<SiCss3/>
+  },
+  {
+    titulo:'PhotoShop',
+    icone:<SiAdobephotoshop/>
+  },
+  {
+    titulo:'Ilustrator',
+    icone:<SiAdobeillustrator/>
+  }
+]
   
+
+  
+//Navigation
+  const [navAtive, setNavAtive] = useState(false)
+
+  function showMenu (){
+    if(navAtive){
+    setNavAtive(false)}
+    else{
+
+    setNavAtive(true)
+    }
+  }
+
+//buttons
+const coder = '<Coder>'
+const [openCoder, setOpenCoder] = useState(false)
+const [openDesigner, setOpenDesigner] = useState(false)
+
+function coderButton (){
+  if(openCoder){
+    setOpenCoder(false)
+  }else{
+    setOpenCoder(true)
+  }
+}
+
+
+function designerButton (){
+  if(openDesigner){
+    setOpenDesigner(false)
+  }else{
+    setOpenDesigner(true)
+  }
+}
+
+
   return (
     
 
     <header className='app'>
       
       <nav className='menu'>
-        <h2 className='assinatura'>Gabriel Tomé</h2>
+
+        <div className='conteinerNav'>
+          <AiOutlineMenu className='nav' onClick={()=> showMenu()}/>
+
+          <h2 className='assinatura'>Gabriel Tomé</h2>
       
 
-      <ul className='smoothScroll'>
-        <li><Link to='sessao1' offset={-55} spy={true}
+          <ul className={navAtive ? 'navAtive' : 'smoothScroll'}>
+            <li><Link to='sessao1' offset={-55} spy={true}
     smooth={true}  duration={500}>O que eu faço?</Link></li>
-        <li><Link to='sessao2' offset={-55} spy={true}
+            <li><Link to='sessao2' offset={-55} spy={true}
     smooth={true}  duration={500}>Projetos</Link></li>
-        <li><Link to='sessao3' spy={true}
-    smooth={true}  duration={500}>Quem sou eu?</Link></li>
-        <li><Link to='sessao4' spy={true}
+            <li><Link to='sessao3' spy={true}
+    smooth={true}  duration={500} offset={-55}>Quem sou eu?</Link></li>
+            <li><Link to='sessao4' spy={true}
     smooth={true}  duration={500}>Fale comigo</Link></li>
-      </ul>
+          </ul>
+
+        </div>
       </nav>
 
       <div className='sessao1'>
-      <h1>O que eu faço?</h1>
-      <div className='animation1'>
-        <img src={cerebro} className='cerebro'/>
-        <div className='buttons'>
-        <button className='coder'><p>{coder}</p></button>
-         <button className='designer'><p>Designer</p></button>
+        
+        <h1>O que eu faço?</h1>
+
+        <div className='animation1'>
+          <img src={cerebro} className='cerebro'/>
+        
+          <div className='buttons'>
+            <button className='coder' onClick={()=> coderButton()}><p>{coder}</p></button>
+            <button className='designer' onClick={()=> designerButton()}><p>Designer</p></button>
+          </div>
         </div>
-      </div>
+
+        <div className={openCoder ? 'openCoder' : 'closeCoder'}>
+
+          
+          {coderSkill.map( skill => {
+            return(
+              <div className='coderSkill'>
+            
+                
+                <h4>{skill.titulo}</h4>
+                {skill.icone} 
+
+              </div>    )
+          })}
+          
+
+        </div>
+
+        <div className={openDesigner ? 'openDesigner' : 'closeDesigner'}>
+
+          
+          {designerSkill.map( skill => {
+            return(
+              <div className='designerSkill'>
+            
+                {skill.icone}
+                <h4>{skill.titulo}</h4> 
+
+              </div>    )
+          })}
+          
+
+        </div>
+
       </div>
 
       <div className='sessao2'>
